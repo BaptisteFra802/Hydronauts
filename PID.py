91954516a0277d61d3c4c2b66b310d305169c9fb
Baptiste Montagnon <baptiste.montagnon@estaca.eu>
2024-02-08 16:04:07 +0100

    Initial Commit

---
+++ PID.py
@@ -0,0 +1,65 @@
+def PID(Kp, Ki, Kd, MV_bar=0, beta=1, gamma=0):
+    P=0
+    I=0
+    D=0
+    eD_prev=0
+    t_prev=0
+
+
+    while True:
+        t,PV,SP,TR = yield MV
+
+        I=TR-MV_bar-P-D
+
+        P=Kp*(beta*SP - PV)
+        I=I+Ki*(SP-PV)*(t-t_prev)
+
+
+        D=Kd*(eD-eD_prev)/(t-t_prev)
+        MV=MV_bar + P + I + D
+
+
+        eD_prev = eD_prev
+        t_prev=t
+
+
+
+
+print(PID())
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.ndimage.filters import gaussian_filter1d
+
+# Update the t_end and n_steps values for 10 iterations
+t_start = 0
+t_end = 500
+n_steps = 50
+dt = (t_end - t_start) / n_steps
+time_values = [t_start + i*dt for i in range(n_steps)]
+# We will use a constant target value (10)
+setpoint_values = [10 for _ in time_values]
+
+# Our starting value is 0, it will be updated in the next steps with the values coming from the PID control
+measurements = [0]
+errors = [setpoint_values[0] - measurements[0]]
+
+
+
+def PID2(Kp,Ki,Kd):
+    val_P=[0]
+    val_I=[0]
+    val_D=[0]
+    val_PID=[0]
+
+    I=0
+    erreur_ant=0
+
+
+
+    for i in range(1,n):
+        e=erreur[-1]
+        I=I+dt*e
+        
---
+++ teleop_keyboard.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+from math import pi
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Float64
+from ros_gz_interfaces.msg import ParamVec
+
+
+import sys
+
+class TeleopKeyboard(Node):
+    def __init__(self):
+        super().__init__('teleop_keyboard')
+        self.bearing=0
+        self.range=0
+        self.phase=1
+
+        self.main_turn_pub = self.create_publisher(Float64, '/wamv/thrusters/main/pos', 5)
+        self.main_speed_pub = self.create_publisher(Float64, '/wamv/thrusters/main/thrust', 5)
+        self.main_bouee=self.create_subscription(ParamVec, '/wamv/sensors/acoustics/receiver/range_bearing', self.callback, 5)
+
+        self.main_phase=self.create_subscription(ParamVec,'/vrx/patrolandfollow/current_phase',self.callback_phase,5)
+
+        self.get_logger().info('Teleop Keyboard Node Started')
+        self.main_timer=self.create_timer(0.1,self.run)
+
+
+    def callback(self,msg):
+        for param in msg.params:
+            if param.name=='bearing':
+                self.bearing=param.value.double_value
+            if param.name=='range':
+                self.range=param.value.double_value
+
+
+    def callback_phase(self,msg):
+        for paramphase in msg.data:
+            return(paramphase)
+
+
+    def dir(self):
+        gain=0.01
+        return(min((pi/4),max((-pi/4),-gain*self.bearing)))
+
+    def moteur(self):
+        gain=50
+        x=gain*self.range
+        return(max(0,min(12000,x)))
+
+
+
+    def vels(self, speed, turn):
+        return "currently:\tspeed %s\tturn %s " % (speed,turn)
+    
+    def run(self):
+        speed = 0.0
+        turn = 0.0
+        speed_limit = 12000.0
+        turn_limit = 0.78539816339
+        try:
+            speed=float(self.moteur())
+            turn=float(self.dir())
+
+            print(self.vels(speed,turn))
+
+            print(self.phase())
+
+            speed_msg = Float64()
+            turn_msg = Float64()
+            speed_msg.data = speed
+            turn_msg.data = turn
+            
+            self.main_turn_pub.publish(turn_msg)
+            self.main_speed_pub.publish(speed_msg)
+
+        except Exception as e:
+            print(e)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    teleop_keyboard = TeleopKeyboard()
+
+    rclpy.spin(teleop_keyboard)
+    
+    teleop_keyboard.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
